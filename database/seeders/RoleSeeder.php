0b33113ec8a2760d4e21afa0d79e43728015f06e
SamGIraldoP <260954305+SamGIraldoP@users.noreply.github.com>
2026-05-31 11:30:00 -0500

    feat(auth): migraciones de users y roles + rol Usuario por defecto (HU-ADMI-004)

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create(['type' => 'Administrador']);
+        Role::create(['type' => 'Usuario']);
+    }
+}
